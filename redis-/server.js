5e3b5d3acb0f3a9a5ec4884f6712e5e52329e51c
NABADEEP KR. DAS <nabadeepdas069@gmail.com>
2025-07-04 20:35:17 +0530

    under. redis

---
+++ redis-/server.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws');
+const redis = require('redis');
+
+
+const wss = new WebSocket.Server({ port: 8080 }, () => {
+  console.log('WebSocket server started on ws://localhost:8080');
+});
+
+
+const subscriber = redis.createClient();
+const publisher = redis.createClient();
+
+
+subscriber.subscribe('mychannel');
+
+subscriber.on('message', (channel, message) => {
+  console.log(`Redis: ${message} on ${channel}`);
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+});
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received from client: ${message}`);
+    
+    publisher.publish('mychannel', message);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
